68bf1b9228ac53ff812231c2b6d0394632d7768f
KimDoHyun1011 <hot99009@gmail.com>
2024-03-07 20:42:39 +0900

    struct study2

--- work/main08.cpp
+++ work/main08.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 
-//구조체(struct) - user Define DataType
+// 구조체(struct) - user Define DataType
+// 클래스(class) 
 
 struct MyDataType
 {
-
+	float f;
+	int i;
 };
 
 
 int main()
 {
 	MyDataType t;
+	t.f;
+	t.i;
 	MyDataType a;
+	
+	
 
 	return 0;
 }
