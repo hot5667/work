c8a898a9b0e5f6c0aa21735c7af66aa5849a7db6
KimDoHyun1011 <hot99009@gmail.com>
2024-03-07 20:46:16 +0900

    struct study3

--- work/main08.cpp
+++ work/main08.cpp
@@ -3,10 +3,11 @@
 // 구조체(struct) - user Define DataType
 // 클래스(class) 
 
-struct MyDataType
+struct MyDataType //12바이트 인 이유 제일 큰 바이트 단위 로 계산 하기 떄문에 12바이트 으로 확인이 된다.
 {
 	float f;
 	int i;
+	short s;
 };
 
 
@@ -15,7 +16,10 @@ int main()
 	MyDataType t;
 	t.f;
 	t.i;
-	MyDataType a;
+
+	MyDataType kkk;
+	kkk.f;
+	kkk.i;
 	
 	
 
