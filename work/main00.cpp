ec920a544eb41335b51f003f021b185e48c7b0cd
KimDoHyun1011 <hot99009@gmail.com>
2024-03-07 20:37:15 +0900

    struct study

--- work/main00.cpp
+++ work/main00.cpp
@@ -2,21 +2,34 @@
 
 void arrsort(int(&arr)[10])
 {
-
-	for (int i = 0; i < 9; ++i)
+	for (int j = 0; j < 9; ++j)
 	{
-		if (arr[i] > arr[i + 1])
-		{
-			int temp = arr[0];
-			arr[0] = arr[i + 1];
-			arr[i + 1] = temp;
+		bool bSwap = false;
 
-			// swap 과정 코드
+		for (int i = 0; i < (9 - j); ++i)
+		{
+			if (arr[i] > arr[i + 1])
+			{
+				int temp = arr[0];
+				arr[0] = arr[i + 1];
+				arr[i + 1] = temp;
+
+				// swap 과정 코드
+
+				bSwap = true;
+			}
+		}
+
+		if (false == bSwap)
+		{
+			break;
 		}
 	}
 }
 
 int main()
 {
+	int arrtest[10] = { 56,78,1,45,99,67,2,27,10,9 };
 
+	arrsort(arrtest);
 }
